ef83746593553ae9a44debc336cc80b8a7ebc88b
krzysdz <krzysdz@users.noreply.github.com>
2025-05-14 14:09:43 +0200

    Lab 2 add coverage

--- Lab_2/clearsim.sh
+++ Lab_2/clearsim.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Remove previous results
-rm -rf csrc simv.daidir verdiLog ucli.key
-rm -f simv ucli/key inter.fsdb* .inter.fsdb.* .vcs_checkpoint_* novas* verdi_config_file
+rm -rf csrc simv.daidir verdiLog ucli.key simv.vdb urgReport vdCovLog
+rm -f simv ucli/key inter.fsdb* .inter.fsdb.* .vcs_checkpoint_* novas* verdi_config_file .fsm.sch.verilog.xml cm.log
--- Lab_2/runsym.sh
+++ Lab_2/runsym.sh
@@ -3,9 +3,19 @@ set -ex
 
 ./clearsim.sh
 
+if ! command -v vcs; then
+    echo "vsc command not found. Did you forget to run alias add-synopsys-FEV-all?"
+    exit 1
+fi
+
 if [ $1 == "--gui" ]; then
     vcs -kdb -debug_access+all -f filelist.f
     ./simv -gui
+elif [ $1 == "--cov" ]; then
+    vcs -cm line+tgl+cond+fsm+branch -f filelist.f
+    ./simv -cm line+tgl+cond+fsm+branch
+    urg -dir simv.vdb
+    verdi -cov -covdir simv.vdb
 else
     vcs -f filelist.f
     ./simv

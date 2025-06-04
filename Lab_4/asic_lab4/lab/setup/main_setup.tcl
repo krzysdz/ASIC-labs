040289d2af70525913906028fe1903d167c1dd47
krzysdz <krzysdz@users.noreply.github.com>
2025-06-04 11:10:58 +0200

    Lab 4, ex 2

--- Lab_4/asic_lab4/lab/setup/main_setup.tcl
+++ Lab_4/asic_lab4/lab/setup/main_setup.tcl
@@ -23,24 +23,24 @@ set TechFileDir    		"${TechLibDir}/tf"  ; # directory with technology file(s)
 set TechFile			"${TechFileDir}/${TechNodeName}_${MetalStack}.tf" ; # technology file
 #set TechNdm				""
 
-set LibertyDir			"${TechLibDir}/liberty" ; # 
+set LibertyDir			"${TechLibDir}/liberty" ; #
 
-set TlupDir				"${TechLibDir}/tlup" ; # 
-set TlupMinFile			"${TlupDir}/${TechNodeName}_${MetalStack}_Cmin.tlup" ; # 
-set TlupNomFile			"${TlupDir}/${TechNodeName}_${MetalStack}_Cnom.tlup" ; # 
-set TlupMaxFile			"${TlupDir}/${TechNodeName}_${MetalStack}_Cmax.tlup" ; # 
-set NxtGrdDir			"${TechLibDir}/nxtgrd" ; # 
-set NxtGrdMinFile		"${NxtGrdDir}/${TechNodeName}_${MetalStack}_Cmin.nxtgrd" ; # 
-set NxtGrdNomFile		"${NxtGrdDir}/${TechNodeName}_${MetalStack}_Cnom.nxtgrd" ; # 
-set NxtGrdMaxFile		"${NxtGrdDir}/${TechNodeName}_${MetalStack}_Cmax.nxtgrd" ; # 
-set MapDir				"${TechLibDir}/map" ; # 
-set GdsDir				"${TechLibDir}/gds" ; # 
-set RunsetDir			"${TechLibDir}/runsets" ; # 
-set GdsFile				"${GdsDir}/saed14rvt.gds" ; # 
-set GdsMapFile			"${MapDir}/saed14nm_1p9m_gdsout_mw.map" ; # 
-set LayerMapFile		"${MapDir}/${TechNodeName}_tf_itf_tluplus.map" ; # 
-set DrcRunsetFile		"${RunsetDir}/saed14nm_1p9m_drc_rules.rs" ; # 
-set MetalFillRunsetFile	"${RunsetDir}/saed14nm_1p9m_mfill_rules.rs" ; # 
+set TlupDir				"${TechLibDir}/tlup" ; #
+set TlupMinFile			"${TlupDir}/${TechNodeName}_${MetalStack}_Cmin.tlup" ; #
+set TlupNomFile			"${TlupDir}/${TechNodeName}_${MetalStack}_Cnom.tlup" ; #
+set TlupMaxFile			"${TlupDir}/${TechNodeName}_${MetalStack}_Cmax.tlup" ; #
+set NxtGrdDir			"${TechLibDir}/nxtgrd" ; #
+set NxtGrdMinFile		"${NxtGrdDir}/${TechNodeName}_${MetalStack}_Cmin.nxtgrd" ; #
+set NxtGrdNomFile		"${NxtGrdDir}/${TechNodeName}_${MetalStack}_Cnom.nxtgrd" ; #
+set NxtGrdMaxFile		"${NxtGrdDir}/${TechNodeName}_${MetalStack}_Cmax.nxtgrd" ; #
+set MapDir				"${TechLibDir}/map" ; #
+set GdsDir				"${TechLibDir}/gds" ; #
+set RunsetDir			"${TechLibDir}/runsets" ; #
+set GdsFile				"${GdsDir}/saed14rvt.gds" ; #
+set GdsMapFile			"${MapDir}/saed14nm_1p9m_gdsout_mw.map" ; #
+set LayerMapFile		"${MapDir}/${TechNodeName}_tf_itf_tluplus.map" ; #
+set DrcRunsetFile		"${RunsetDir}/saed14nm_1p9m_drc_rules.rs" ; #
+set MetalFillRunsetFile	"${RunsetDir}/saed14nm_1p9m_mfill_rules.rs" ; #
 
 
 set RtlDir				"${SrcDir}/rtl" ; # directory with source files
@@ -52,6 +52,7 @@ set SdcDir				"${SrcDir}/sdc" ; # directory with constraint files
 
 set ResultsDir			"${LabDir}/results" ; # directory for outcomes of the lab
 set ReportsDir			"${LabDir}/reports" ; # directory for report files
+set SolutionDir         "${LabDir}/solution"
 
 echo					${SetupDir}
 source 					${SetupDir}/utilities.tcl
--- Lab_4/asic_lab4/lab/task2/scripts/task2.tcl
+++ Lab_4/asic_lab4/lab/task2/scripts/task2.tcl
@@ -8,5 +8,25 @@ source -echo  ../../setup/main_setup.tcl
 #### Sourcing project specyfic setup script
 source -echo  ${SetupDir}/design_setup.tcl
 
-exit
+# Create and compile library
+set_app_var link_library "${DB_FF} ${DB_TT} ${DB_SS}"
+create_lib ${ResultsDir}/${DesignLibrary}  -technology ${TechFile} -ref_libs ${RefLib}
+report_ref_libs
 
+# Analyze and elaborate
+analyze -format sverilog [glob ${SystemVerilogDir}/*.{svh,sv}]
+elaborate ${DesignName}
+set_top_module ${DesignName}
+
+# Save as rtl_read and close
+save_block -as ${DesignName}/rtl_read
+list_blocks
+close_blocks -force
+
+# Save lib and close
+save_lib
+close_lib
+
+copy_lib -from_lib ${ResultsDir}/${DesignLibrary} -to_lib "${SolutionDir}/${DesignLibrary}"
+
+exit
